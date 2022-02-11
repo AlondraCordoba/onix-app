63aee5e150006553104156d9b4531ba6c95bc276
AlondraCordoba <alondracordoba0720@gmail.com>
2022-02-11 15:28:27 -0600

    Database changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,9 +98,5 @@ Rails.application.configure do
   end
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  const PORT = process.env.PORT || 5000
-  
-      app.listen(PORT, function() {
-      console.log("Server running on port 5000...");
-      });
+
 end
